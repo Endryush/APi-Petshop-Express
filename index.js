521bacb43bd4f920d926468e12ef4b54cf2ed967
Endryush <endryush@gmail.com>
2022-08-06 08:39:36 -0300

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/tabelas')
-
+const port = 3002
 conexao.connect(erro => {
     if(erro) {
         console.log(erro)
@@ -12,6 +12,6 @@ conexao.connect(erro => {
         
         const app = customExpress()
 
-        app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+        app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
     }
 })
